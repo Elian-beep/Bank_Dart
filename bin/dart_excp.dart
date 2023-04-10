b2bd96b5cbbf3c885bd48ecd70e03871a3203837
Elian-beep <elianoliver101@gmail.com>
2023-04-10 17:00:27 -0300

    created styled exceptions

--- bin/dart_excp.dart
+++ bin/dart_excp.dart
@@ -1,25 +1,39 @@
 import 'controllers/bank_controller.dart';
+import 'exceptions/bank_controller-exceptions.dart';
 import 'models/account.dart';
 
 void main() {
- // Criando o banco
- BankController bankController = BankController();
+  // Criando o banco
+  BankController bankController = BankController();
 
- // Adicionando contas
- bankController.addAccount(
-     id: "Ricarth",
-     account:
-         Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
+  // Adicionando contas
+  bankController.addAccount(
+      id: "Ricarth",
+      account:
+          Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
 
- bankController.addAccount(
-     id: "Kako",
-     account:
-         Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
+  bankController.addAccount(
+      id: "Kako",
+      account:
+          Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
 
- // Fazendo transferência
- bool result = bankController.makeTransfer(
-     idSender: "Kako", idReceiver: "Ricarth", amount: 50);
-
- // Observando resultado
- print(result);
+  // Fazendo transferência
+  try {
+    bool result = bankController.makeTransfer(
+        idSender: "Kako", idReceiver: "Ricarth", amount: 599);
+    // Observando resultado|
+    if (result) {
+      print("Transação concluída com sucesso");
+    }
+  } on SenderIdInvalidException catch (e) {
+    print("O id ${e.idSender} do remetente não é um id válido");
+  } on ReceiverIdInvalidException catch (e) {
+    print("O id ${e.idReceiver} do destinatário não é um id válido");
+  } on SenderNotAuthenticatedException catch (e) {
+    print("o usuário remetente de id ${e.idSender} não esta autenticado");
+  } on SenderBalanceLowerThaAmountException catch (e) {
+    print("O usuário de id ${e.idSender} tentou enviar ${e.amount} sendo que na sua conta tem apenas ${e.senderBalance}");
+  } on Exception {
+    print("As coisas saíram do controle");
+  }
 }
--- bin/exceptions/bank_controller-exceptions.dart
+++ bin/exceptions/bank_controller-exceptions.dart
@@ -1,7 +1,21 @@
-class SenderIdInvalidException implements Exception {}
+class SenderIdInvalidException implements Exception {
+  String idSender;
+  SenderIdInvalidException({required this.idSender});
+}
 
-class ReceiverIdInvalidException implements Exception{}
+class ReceiverIdInvalidException implements Exception{
+  String idReceiver;
+  ReceiverIdInvalidException({required this.idReceiver});
+}
 
-class SenderNotAuthenticatedException implements Exception{}
+class SenderNotAuthenticatedException implements Exception{
+  String idSender;
+  SenderNotAuthenticatedException({required this.idSender});
+}
 
-class SenderBalanceLowerThaAmountException implements Exception{}
+class SenderBalanceLowerThaAmountException implements Exception{
+  String idSender;
+  double senderBalance;
+  double amount;
+  SenderBalanceLowerThaAmountException({required this.idSender, required this.senderBalance, required this.amount});
+}
